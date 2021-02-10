e03243ab657820291054c73bfc34044a9793cebf
malgbogd <malgbogd@gmail.com>
2021-02-10 18:49:31 +0100

    modal working, crating new project working

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,10 @@
 const allProjects = [];
+let newProject;
+
+let newProjectName = document.getElementById("projectName");
+let newProjectDescription = document.getElementById("projectDescription"); 
+let newProjectDeadline = document.getElementById("projectDeadline");
+let newProjectChecklist = document.getElementById("projectChecklist");
 
 const page = document.getElementById("content");
 
@@ -8,20 +14,21 @@ const createBtnAddProject = (()=> {
     addProject.innerHTML = "+ add new project";
     addProject.id = "addProjBtn";
     page.appendChild(addProject);
+    addProject.addEventListener("click", ()=> {
+        document.getElementById("addProjWin").style.display = "block";
+    });
 })();
 
-const createProject = (name, description, deadline, checklist) => {
+const project = (name, description, deadline, checklist) => {
+    
     const getName = () => name;
     const getDesription = () => description;
     const getDeadline = () => deadline;
     const getChecklist = () => checklist;
 
-    let id = allProjects.length;
-    allProjects[id] = this; //Czy to będzie działać nie w konstruktrze?
-
-     setName = (name) => {
-         this.name = name;
-        return this;
+    setName = (name) => {
+      this.name = name;
+      return this;
     };
 
     setDescription = (description) => {
@@ -38,46 +45,67 @@ const createProject = (name, description, deadline, checklist) => {
         this.checklist = checklist;
         return this;
     };
+
+    return {name,description,deadline,checklist, getName, getDesription, getDeadline, getChecklist, setName, setDescription, setDeadline, setChecklist}
+
 };
 
-
-// pierwsze próby stworzenia modala
 
 const createModal = (() => {
 
     const modal = document.createElement("div");
     modal.id = "addProjWin"
 
+    const winTitle = document.createElement("div");
+    winTitle.id = "winTitle";
+    winTitle.innerHTML = "Add New Project"
+
+    const closeDiv = document.createElement("div");
+    closeDiv.id ="closeDiv";
+
     const closeSign = document.createElement("span");
     closeSign.innerHTML = "&times;";
-    closeSign.className = "close"
+    closeSign.className = "close";
+    closeSign.addEventListener("click", () =>{
+        modal.style.display = "none";
+    });
+
+    closeDiv.appendChild(closeSign);
 
     const nameTag = document.createElement("div");
     nameTag.innerHTML = "Project name";
+    nameTag.className = "modalTag";
 
     const nameInput = document.createElement("input");
     nameInput.id = "projectName";
     nameInput.type = "text";
+    nameInput.className = "modalInput";
 
     const descriptionTag = document.createElement("div");
     descriptionTag.innerHTML = "Description";
+    descriptionTag.className = "modalTag";
 
     const descriptionInput = document.createElement("input");
     descriptionInput.id = "projectDescription";
     descriptionInput.type = "text";
+    descriptionInput.className = "modalInput";
 
     const deadlineTag = document.createElement("div");
     deadlineTag.innerHTML = "Deadline";
+    deadlineTag.className = "modalTag";
 
     const deadlineInput = document.createElement("input")
     deadlineInput.id = "projectDeadline";
     deadlineInput.type = "date";
+    deadlineInput.className = "modalInput";
 
     const checklistDiv = document.createElement("div");
     checklistDiv.id = "checklistDiv";
+    checklistDiv.className = "modalInput";
 
     const checklistTag = document.createElement("div");
-    checklistTag.innerHTML = "Do you want to add checklist?"
+    checklistTag.innerHTML = "Add checklist?"
+    checklistTag.className = "modalTag";
     
     const checklistInput =document.createElement("Input");
     checklistInput.id = "projectChecklist";
@@ -87,9 +115,11 @@ const createModal = (() => {
     checklistDiv.appendChild(checklistInput);
     
     const addBtnWin = document.createElement("button");
-    addBtnWin.innerHTML= "Add"
+    addBtnWin.innerHTML= "Add";
+    addBtnWin.id ="add";
 
-    modal.appendChild(closeSign);
+    modal.appendChild(closeDiv);
+    modal.appendChild(winTitle);
     modal.appendChild(nameTag);
     modal.appendChild(nameInput);
     modal.appendChild(descriptionTag);
@@ -99,4 +129,31 @@ const createModal = (() => {
     modal.appendChild(checklistDiv);
     modal.appendChild(addBtnWin);
     document.body.appendChild(modal);
+    
+    return [nameInput, descriptionInput, deadlineInput, checklistInput];
 })();
+
+document.getElementById("add").addEventListener("click", ()=>{
+    getInput()
+    newProject = project(newProjectName,newProjectDescription,newProjectDeadline, newProjectChecklist);
+    allProjects.push(newProject);
+    console.log(allProjects);
+    clearInput(); 
+    return newProject;
+});
+    
+    
+function getInput() {
+newProjectName = document.getElementById("projectName").value;
+newProjectDescription = document.getElementById("projectDescription").value; 
+newProjectDeadline = document.getElementById("projectDeadline").value;
+newProjectChecklist = document.getElementById("projectChecklist").value;
+return [newProjectName, newProjectDescription, newProjectDeadline, newProjectChecklist];
+}
+
+function clearInput() {
+    newProjectName.value = "";
+    newProjectDescription.value = "";
+    newProjectDeadline.value= "";
+    newProjectChecklist.value = "off";
+}
